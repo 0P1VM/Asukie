8d10d363ad43b10c7cb2747e13d78e3db629d4c0
Glitch (commands-test-canary) <none>
2020-10-09 16:49:30 +0000

    🚔📼 Updated

---
+++ commands/covidsin.js
@@ -0,0 +1,23 @@
+const Discord = require('discord.js')
+
+module.exports = {
+    name: 'covidsin',
+	description: 'Sintomas do COVID-19',
+    aliases: ['coronasintoma', 'c19sin'],
+    usage: '$covidsin',
+    cooldown: 5,
+	run: async (client, message, args) => {
+    
+    let covidsin = new Discord.MessageEmbed()
+    .setTitle("Sintomas do COVID-19")
+    .addField("Sintomas mais comuns", " Febre\n Tosse\n  Cansaço")
+    .addField("Sintomas menos comuns", " Dores e desconfortos\n  Dor de garganta\n  Diarreia\n  Conjutivite\n  Dor de cabeça\n  Perda de paladar\n  Erupção cutânea na pele ou descoloração dos dedos das mãos ou dos pés")
+    .addField("Sintomas graves", " Dificuldade de respirar ou falta de ar\n  Dor ou pressão no peito\n  Perda de fala ou movimento")
+    .setColor('#8500de')
+    .setThumbnail('https://images.vexels.com/media/users/3/193239/isolated/preview/e83db85552ee35f6276411c9f19d982d-covid-19-boy-character-icon-by-vexels.png')
+    .setFooter(`Requisitado: ${message.author.username}`, message.author.displayAvatarURL({dynamic: true}))
+
+    message.channel.send(covidsin)
+
+    }
+}
--- commands/help.js
+++ commands/help.js
@@ -16,12 +16,12 @@ message.delete();
     `<a:tres_:763772976168304650> | Configuração | MANUTENÇÃO\n` +
 		`<a:quatro_:763773033415442523> | Interação\n` +
     `<a:cinco_:763773112995020860> | Utilidades | MANUTENÇÃO**`)
-    .setFooter(`Página 0 de 5 | Solicitado por: ${message.author.username}`, message.author.displayAvatarURL({ dynamic:true }))
+    .setFooter(`Página 0 de 5 | Requisitado: ${message.author.username}`, message.author.displayAvatarURL({ dynamic:true }))
 	.setThumbnail(client.user.displayAvatarURL({ dynamic:true }))
 .setImage('https://cdn.discordapp.com/attachments/760597034138075158/761697500515270706/windows_banner.png')
 
      message.channel.send(painel).then(msg => {
-       msg.react('762496132092788767').then(() => msg.react('763772746386898964')).then(() => msg.react('763772765412393010')).then(() => msg.react('763772976168304650')).then(() => msg.react('763773033415442523')).then(() => msg.react('763773112995020860'))
+       msg.react('763948077970489366').then(() => msg.react('763772746386898964')).then(() => msg.react('763772765412393010')).then(() => msg.react('763772976168304650')).then(() => msg.react('763773033415442523')).then(() => msg.react('763773112995020860'))
        
       let adm = (reaction, usuario) => reaction.emoji.id === "763772746386898964" && usuario.id === message.author.id;
       let coletor = msg.createReactionCollector(adm);
@@ -35,7 +35,7 @@ message.delete();
 	 `<:setinha:763773726419583017> **Removecargo** - Retire um cargo de algum usuário.\n` +
 	 `<:setinha:763773726419583017> **Cargoall** - Adicione algum cargo, para todos dentro do servidor.\n` +
 	 `<:setinha:763773726419583017> **Votacao** - Faz uma votação no seu servidor(obs: use o comando no chat onde você irá fazer a votação.)`)
-	 .setFooter(`Página 1 de 5 | Solicitado por: ${message.author.username}`, message.author.displayAvatarURL({ dynamic:true }))
+	 .setFooter(`Página 1 de 5 | Requisitado: ${message.author.username}`, message.author.displayAvatarURL({ dynamic:true }))
 	 .setThumbnail(client.user.displayAvatarURL({ dynamic:true }))
    .setImage('https://cdn.discordapp.com/attachments/760597034138075158/761697500515270706/windows_banner.png')
       coletor.on("collect", cp => {
@@ -63,7 +63,7 @@ message.delete();
 `<:setinha:763773726419583017> **Kick** - Expulse algum membro do seu servidor.\n` +
 `<:setinha:763773726419583017> **Warn** - De um aviso pra algum usuário.\n` +
 `<:setinha:763773726419583017> **Allunban** - Desbani todos os usuários banidos do seu servidor.`)
- .setFooter(`Página 2 de 5 | Solicitado por: ${message.author.username}`, message.author.displayAvatarURL({ dynamic:true }))
+ .setFooter(`Página 2 de 5 | Requisitado: ${message.author.username}`, message.author.displayAvatarURL({ dynamic:true }))
  .setThumbnail(client.user.displayAvatarURL({ dynamic:true }))
   .setImage('https://cdn.discordapp.com/attachments/760597034138075158/761697500515270706/windows_banner.png')
      coletor2.on("collect", cp => {
@@ -87,7 +87,7 @@ message.delete();
  `<:setinha:763773726419583017> **Imunidade** - Selecione chat/cargo, que ser alguma defesa estiver ativada, o bot não irá excluir.\n` +
  `<:setinha:763773726419583017> **Sugestão** - Configure algum chat para sugestões dos membros.\n` +
  `<:setinha:763773726419583017> **Contador** - Coloque no mínimo 10 chats, onde o bot irá contar os membros do servidor todo.`)
-  .setFooter(`Página 3 de 5 | Solicitado por: ${message.author.username}`, message.author.displayAvatarURL({ dynamic:true }))
+  .setFooter(`Página 3 de 5 | Requisitado: ${message.author.username}`, message.author.displayAvatarURL({ dynamic:true }))
   .setThumbnail(client.user.displayAvatarURL({ dynamic:true }))
   .setImage('https://cdn.discordapp.com/attachments/760597034138075158/761697500515270706/windows_banner.png')
       coletor3.on("collect", cp => {
@@ -105,28 +105,24 @@ message.delete();
      .setDescription(`<a:gatinho_:763776921326190655>**・Prefixo:** \`${c.prefix}\`\n` +
  `\n<:setinha:763773726419583017> **Cocegas** - Faça cócegas em algum amiguinho.\n` +
  `<:setinha:763773726419583017> **Primeiraspalavras** - Primeiras palavras de um bebê.\n` +
- `<:setinha:763773726419583017> **Say** - Escreva algo como se fosse a própria Asukie escrevendo.\n` +
  `<:setinha:763773726419583017> **Kiss** - Beije seu crush.\n` +
  `<:setinha:763773726419583017> **Neko** - Veja melhores gifs de neko.\n` +
  `<:setinha:763773726419583017> **Ship** - Faça um ship de algum casal.\n` +
  `<:setinha:763773726419583017> **Cafune** - Faça um cafuné em algum amigo(a).\n` +
- `<:setinha:763773726419583017> **Covid** - Veja os casos de Covid no mundo inteiro.\n` +
  `<:setinha:763773726419583017> **Tapa** - De um tapa em alguém.\n` +
  `<:setinha:763773726419583017> **Mcskin** - É um comando pra você conseguir a skin de alguma conta do Minecraft.\n` +
  `<:setinha:763773726419583017> **Mchead** - É um comando pra você conseguir a Cabeça de alguma conta do Minecraft.\n` +                   
  `<:setinha:763773726419583017> **Conquista** - Use o comando, e conquiste oque mais deseja.\n` +
  `<:setinha:763773726419583017> **Matar** - Mate alguém. **(Leve o comando na zoeira)**\n` +
  `<:setinha:763773726419583017> **Hackear** - "hackei" alguém. **(Leve o comando na zoeira)**\n` +
- `<:setinha:763773726419583017> **Ascii** - Escreva em modo Ascii.\n` +
  `<:setinha:763773726419583017> **Duvida** - Deixa eu tirar uma dúvida sua.\n` +
  `<:setinha:763773726419583017> **Hug** - De um abraço em algum amigo(a).\n` +                   
  `<:setinha:763773726419583017> **Cronometro** - Irei cronometrar um tempo pra você.\n` +                    
- `<:setinha:763773726419583017> **Bigtext** - Escreva com letras grandes.\n` +
  `<:setinha:763773726419583017> **Clima** - Veja o clima de alguma cidade.\n` +                   
  `<:setinha:763773726419583017> **Dado** - Jogue dado comigo.\n` +
  `<:setinha:763773726419583017> **Roletarussa** - Jogue roletarussa comigo.\n` +                   
  `<:setinha:763773726419583017> **Baka** - Chame alguém de baka.`)
-  .setFooter(`Página 4 de 5 | Solicitado por: ${message.author.username}`, message.author.displayAvatarURL({ dynamic:true }))
+  .setFooter(`Página 4 de 5 | Requisitado: ${message.author.username}`, message.author.displayAvatarURL({ dynamic:true }))
   .setThumbnail(client.user.displayAvatarURL({ dynamic:true }))
   .setImage('https://cdn.discordapp.com/attachments/760597034138075158/761697500515270706/windows_banner.png')
       coletor4.on("collect", cp => {
@@ -146,6 +142,8 @@ message.delete();
      `<:setinha:763773726419583017> **Cpu** - Veja minhas configurações de cpu.\n`+
      `<:setinha:763773726419583017> **Avatar** - Veja seu avatar em um tamanho maior ou de outro membro.\n` +
      `<:setinha:763773726419583017> **Rankinvite** - Veja o rank de invite.\n` +
+     `<:setinha:763773726419583017> **Covid** - Veja os casos do Covid-19 no mundo inteiro.\n` +
+     `<:setinha:763773726419583017> **Covidsin** - Veja os sintomas do Covid-19.\n` +                
      `<:setinha:763773726419583017> **Uptime** - Veja o tempo do bot online.\n` +
      `<:setinha:763773726419583017> **Botinfo** - Veja informações do bot toda.\n` +
      `<:setinha:763773726419583017> **Ping** - Veja a velocidade da mensagem do bot.\n` + 
@@ -154,7 +152,7 @@ message.delete();
      `<:setinha:763773726419583017> **Userinfo** - Veja informações de algum usuário.\n` +
      `<:setinha:763773726419583017> **Emojiinfo** - Veja informações de algum emoji do seu servidor.\n` +
      `<:setinha:763773726419583017> **Emoji** - Veja algum emoji em um tamanho maior, pode ser de outros servidores também.`)
-  .setFooter(`Página 5 de 5 | Solicitado por: ${message.author.username}`, message.author.displayAvatarURL({ dynamic:true }))
+  .setFooter(`Página 5 de 5 | Requisitado: ${message.author.username}`, message.author.displayAvatarURL({ dynamic:true }))
   .setThumbnail(client.user.displayAvatarURL({ dynamic:true }))
   .setImage('https://cdn.discordapp.com/attachments/760597034138075158/761697500515270706/windows_banner.png')
       coletor5.on("collect", cp => {
